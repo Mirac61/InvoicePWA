6770633abdd1b07c18d6f030b70c214ffcde9be5
quantumsine <thelinn.sp@gmail.com>
2026-04-10 10:56:07 +0200

    style: Navbar zu Main.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import Navbar from './components/Navbar.vue';
+import Main from './components/Main.vue';
 </script>
 
 <template>
-  <Navbar />
+  <Main />
 </template>
 <style scoped>
 </style>
